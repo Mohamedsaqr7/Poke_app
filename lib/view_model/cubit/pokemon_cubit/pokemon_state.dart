89075826da32c38d7537daeba65ddbc1903ee05e
shekosakr <mmdsakr942@gmail.com>
2023-09-25 21:40:54 +0300

    first commit

---
+++ lib/view_model/cubit/pokemon_cubit/pokemon_state.dart
@@ -0,0 +1,6 @@
+abstract class Pokestate {}
+class Initstate extends Pokestate{}
+class PokemonLoadingstate extends Pokestate{}
+class PokemonSuccessstate extends Pokestate{}
+class PokemonErrorstate extends Pokestate{}
+class imagemove extends Pokestate{}
